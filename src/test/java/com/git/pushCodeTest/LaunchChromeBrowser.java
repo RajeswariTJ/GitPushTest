5d3bdb40b771f7deff6688b04c1014dbc5b64aab
RajeswariTJ <rajicse379@gmail.com>
2021-07-03 12:19:11 +0530

    first commit

---
+++ src/test/java/com/git/pushCodeTest/LaunchChromeBrowser.java
@@ -0,0 +1,16 @@
+package com.git.pushCodeTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchChromeBrowser {		
+		@Test
+		public void launchChromeTest() {
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver = new ChromeDriver();
+			driver.get("http://leaftaps.com/opentaps");
+			}
+
+}
